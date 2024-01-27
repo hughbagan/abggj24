69dc79e6968d7ef102f87f2cf6f616b2a03620c4
hughbagan <hbagan@ualberta.ca>
2024-01-27 15:28:14 -0700

    Merge menus
    Squashed commit of the following:
    
    commit 80507def81fecc5ccac2706c1dd11ae80dd3dadd
    Merge: f1ab3e2 9079802
    Author: hughbagan <hbagan@ualberta.ca>
    Date:   Sat Jan 27 15:27:09 2024 -0700
    
        Merge branch 'master' into menus
    
    commit f1ab3e2d0c8d55829541f80fc0c9e220f1e0dbc8
    Author: Coach Chuck <christian.krueger.health@gmail.com>
    Date:   Sat Jan 27 14:54:25 2024 -0700
    
        menuing working

--- scenes/PauseManager.gd
+++ scenes/PauseManager.gd
@@ -3,24 +3,25 @@ extends Node
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS # exclude from pause
 
-func _input(event):
-	if event is InputEventMouseButton:
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
-			# Resume...
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-			get_tree().paused = false
-
 func _process(delta):
 	if Input.is_action_just_pressed("exit"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			pause()
 		elif Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
-			get_tree().quit()
+			resume()
+			# get_tree().quit()
 	if Input.is_action_pressed("restart"):
 		get_tree().reload_current_scene()
 		get_tree().paused = false
+
+func resume():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_tree().paused = false
+	get_node("/root/World/PauseLayer/PauseMenu").hide()
 
 func pause():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	# PAUSE functionality goes here...
 	get_tree().paused = true
+	
+	get_node("/root/World/PauseLayer/PauseMenu").show()
